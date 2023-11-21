bce837f2ce03cc3a28a247ea262dce58e619c128
Ari Said Maulana <ariisaiid@gmail.com>
2023-11-21 09:01:37 +0700

    initial commit

---
+++ reservation/Increment.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue'
+
+/* Increment */
+const number = ref(0);
+const increment = () => {
+  number.value++
+}
+
+/* Route Emit */
+const emit = defineEmits(['emit']);
+const routeTo = () => {
+  emit('emit');
+};
+
+/* Show Alert Button */
+const showAlert = () => {
+  alert('hello world');
+};
+
+
+</script>
+
+<template>
+  <h1>{{ number }}</h1>
+  <button style="margin-right: 10px;" @click="increment">increment</button>
+  <button @click="routeTo">Route</button>
+
+  <slot name="buttonCustom">
+    <button @click="showAlert" style="background: green;">
+      click me
+    </button>
+  </slot>
+</template>
